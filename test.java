88debd55df5d2fe20a311826ac578fa414926b19
N0xx77 <abodhdpanchal@gmail.com>
2026-04-02 21:33:56 +0530

    connection establishment

--- test.java
+++ test.java
@@ -1,6 +1,28 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class test {
 
     public static void main(String [] args){
-        System.out.println("x");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");   
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error");
+        }
+
+        String url = "jdbc:mysql://localhost:3306/lost_and_found";
+        String user = "root";
+        String password = "root";
+
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Successful connection"); 
+        } catch (SQLException e) {
+            System.out.println("Fail connection"); 
+        }
     }
 }
